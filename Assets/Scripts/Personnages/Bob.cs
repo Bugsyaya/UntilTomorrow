7c25db91c38f0130110b8caaf6040d2426a573d8
Marina <marina.blin@free.fr>
2016-01-30 14:03:37 +0100

    Add file script for Bob and Wendy

---
+++ Assets/Scripts/Personnages/Bob.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bob : MonoBehaviour 
+{
+	public GameObject bob;
+
+	// Use this for initialization
+	void Start () {
+		bob = new GameObject ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Personnages/Wendy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wendy : MonoBehaviour {
+
+	public GameObject wendy;
+
+	// Use this for initialization
+	void Start () {
+		wendy = new GameObject ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("Horizontal"))
+			print("space key was pressed");
+	}
+}
