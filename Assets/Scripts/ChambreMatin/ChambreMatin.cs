44c0e4f502674fc4808ac86bd13b8bfed5c7d5c2
Valentin Cocaud <valentin@cocaud.com>
2016-01-30 19:44:30 +0100

    Add fadding on all scenes and begining of enchainement

---
+++ Assets/Scripts/ChambreMatin/ChambreMatin.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChambreMatin : MonoBehaviour
+{
+    public GameObject ui;
+
+    private Fading fader;
+
+	// Use this for initialization
+	void Start () {
+        fader = ui.GetComponent<Fading>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnColliderEnter2D(Collider2D other)
+    {
+        fader.LoadScene("CusineSalonMatin");
+        Debug.Log("Trigger");
+    }
+}
--- Assets/Scripts/Personnages/Bob.cs
+++ Assets/Scripts/Personnages/Bob.cs
@@ -1,22 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+ [RequireComponent(typeof(Rigidbody2D))]
 public class Bob : MonoBehaviour 
 {
 	public GameObject bob;
 
+    private Rigidbody2D _body;
+
 	public int health = 100;
 	public int speed = 5;
 
-	// Use this for initialization
-	void Start () {
+    void Awake ()
+    {
+        _body = GetComponent<Rigidbody2D>();
+    }
+    // Use this for initialization
+
+    void Start () {
 		bob = new GameObject ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetAxis("Horizontal") != 0){
-			transform.Translate (new Vector3 (Input.GetAxis ("Horizontal") * speed * Time.deltaTime, 0, 0));
+			_body.MovePosition (_body.position + new Vector2 (Input.GetAxis ("Horizontal") * speed * Time.deltaTime, 0));
 		}
 	}
 }
